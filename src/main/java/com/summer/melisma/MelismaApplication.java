d61602bf3e2683235861f42e7e46bf5a24c1267b
fkakd1205 <fkakd0102@naver.com>
2022-03-09 21:31:17 +0900

    first commit

---
+++ src/main/java/com/summer/melisma/MelismaApplication.java
@@ -0,0 +1,13 @@
+package com.summer.melisma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelismaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelismaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/summer/melisma/MelismaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer.melisma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelismaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
