c1aae8ed9ec5b2f80cab318c1ca2946bb4b42664
YeonJun Cho <micayel1219@gmail.com>
2022-03-30 14:37:31 +0900

    28th feat : MusicMapper Interface 생성

---
+++ src/main/java/com/summer/melisma/mapper/MusicMapper.java
@@ -0,0 +1,17 @@
+package com.summer.melisma.mapper;
+
+import com.summer.melisma.model.dto.MusicDto;
+import com.summer.melisma.model.entity.MusicEntity;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface MusicMapper {
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void changeMusicEntityFromDto(MusicDto musicDto, @MappingTarget MusicEntity musicEntity);
+
+    void updateMusicEntityFromDto(MusicDto musicDto, @MappingTarget MusicEntity musicEntity);
+}
